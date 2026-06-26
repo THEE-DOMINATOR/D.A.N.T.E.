07315026d19371575163ebeac20ec9c6bd2be782
THEE-DOMINATOR <ddirado09@gmail.com>
2026-06-26 17:29:38 -0400

    minor bug fixes and documentation update

--- backend/think.py
+++ backend/think.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path=Path("../.env"))
 
 THINKING_MODEL_MODE = os.getenv("THINKING_MODEL_MODE")
-if THINKING_MODEL_MODE is "OLLAMA_CLOUD":
+if THINKING_MODEL_MODE == "OLLAMA_CLOUD":
     try:
         messages = [
             {"role": "system", "content": "You are D.A.N.T.E., a helpful and friendly AI assistant. You are capable of performing a variety of tasks, including answering questions, providing information, and assisting with various activities. You are designed to be user-friendly and provide accurate and helpful responses."},
@@ -19,32 +19,46 @@ if THINKING_MODEL_MODE is "OLLAMA_CLOUD":
         import ollama
         OLLAMA_API_KEY = os.getenv("OLLAMA_API_KEY")
         OLLAMA_CLOUD_MODEL = str(os.getenv("OLLAMA_CLOUD_MODEL"))
-        if OLLAMA_CLOUD_MODEL in ollama.list():
-            pass
-        else:
-            ollama.pull(OLLAMA_CLOUD_MODEL)
-        global getResponse
-        def getResponse(prompt):
-            client  = ollama.Client(
+
+        
+        client  = ollama.Client(
                 host = 'https://ollama.com',
                 headers = {'Authorization': f'Bearer {OLLAMA_API_KEY}'}
             )
+        
+        available_models = [model["model"] for model in client.list()["models"]]
+        
+        if OLLAMA_CLOUD_MODEL not in available_models:
+            ollama.pull(OLLAMA_CLOUD_MODEL)
+
+        def getResponse(prompt):
+            
             messages.append({
                 "role": "user",
                 "content": prompt
             })
+
             full_response = ""
+
             for part in client.chat(OLLAMA_CLOUD_MODEL, messages=messages, stream = True):
                 yield part.message.content
                 full_response += str(part.message.content)
-                messages.append({
-                    "role": "bot",
-                    "content": full_response
-                })
+
+            messages.append({
+                "role": "assistant",
+                "content": full_response
+            })
+            
     except Exception as e:
         if isinstance(e, ImportError):
             raise RuntimeError("You need to install the Ollama python library. Open a terminal and run \"pip install ollama\".")
         else:
             raise RuntimeError(f"An unknown error occurred while setting up Ollama cloud: {e}")
-    
+        
+else:
+    def getResponse(prompt):
+        yield "Something went wrong with your model configuration. A model was either not specified or is not yet implemented. Please check your .env file and try again."
+        print("Something went wrong with your model configuration. A model was either not specified or is not yet implemented. Please check your .env file and try again.")
 
+for chunk in getResponse("hi"):
+    print(chunk, end="", flush=True)
---
+++ docs/think.md
@@ -0,0 +1,19 @@
+# think.py
+This file is responsible for **all** of D.A.N.T.E.'s thinking. It provides an abstraction layer for the system, so that the call made is **always** think.getResponse. Simply put, no matter which model you use, the function called will be getResponse. 
+
+You can make your own model schema like so: 
+```python
+    elif THINKING_MODEL_MODE == your model:
+        # any initial setup needed goes here
+        # this is stuff like API keys and imports
+        def getResponse(prompt):
+            # logic to get response using prompt
+            yield response 
+```
+> [!WARNING]
+> Failing to enclose your schema in an elif statement or include try...except protections can cause D.A.N.T.E. to crash and burn. Be careful. 
+> [!TIP] 
+> You should generally import needed libraries within your if statement. This helps save resources. 
+
+## Contributing
+If you have thoroughly tested your schema, feel free to submit it as a PR on GitHub! 
